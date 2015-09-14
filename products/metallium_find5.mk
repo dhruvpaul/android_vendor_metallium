bb5e5c9d518c2c13af77f8866e1d09814df18f56
MardonHH <markusmenzel@gmail.com>
2015-09-14 20:20:50 +0200

    Update metallium_find5.mk

--- products/metallium_find5.mk
+++ products/metallium_find5.mk
@@ -1,3 +1,5 @@
+ifeq (metallium_find5,$(TARGET_PRODUCT))
+
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := metallium_xxhdpi
 
@@ -13,3 +15,5 @@ PRODUCT_NAME := metallium_find5
 PRODUCT_DEVICE := find5
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/oppo_12069/FIND5:4.1.1/JRO03L/1362469752:user/release-keys PRIVATE_BUILD_DESC="msm8960-user 4.1.1 JRO03L eng.oppo.20130328.172033 release-keys"
+
+endif
