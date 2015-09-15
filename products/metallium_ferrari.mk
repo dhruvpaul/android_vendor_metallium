1b564beee124206f027983b84aa68c055d1ac00c
MardonHH <markusmenzel@gmail.com>
2015-09-15 16:41:02 +0200

    Create metallium_ferrari.mk

---
+++ products/metallium_ferrari.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (metallium_ferrari,$(TARGET_PRODUCT))
+
+$(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
+
+# Include Metallium common configuration
+include vendor/metallium/main.mk
+
+# Inherit some common Metallium stuff.
+$(call inherit-product, vendor/metallium/configs/telephony.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ferrari
+PRODUCT_NAME := metallium_ferrari
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 4i
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build prop overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="ferrari" \
+    PRODUCT_NAME="ferrari" \
+    BUILD_FINGERPRINT="Xiaomi/ferrari/ferrari:5.1.1/LMY48B:userdebug/test-keys" \
+    PRIVATE_BUILD_DESC="ferrari-userdebug 5.1.1 LMY48B test-keys"
+
+endif
